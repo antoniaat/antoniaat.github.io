f105fa7903ba284afe261c99ac6d99844c57f9fe
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-30 13:56:41 +0200

    Updated header section

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,28 +1,32 @@
 import Flip from 'react-reveal/Flip';
 import avatarImg from '../../assets/icons/avatar.svg';
+import headerImg from '../../assets/images/header.svg';
 import { PROFESSION, WEBSITE_TITLE } from '../../scripts/constants/constants';
 
 import {
-  wrapper, title, subtitle, green, image,
+  wrapper, title, subtitle, green, avatar, image,
 } from '../../styles/components/header/header.module.scss';
 
 const Header = () => (
   <header className={wrapper}>
-    <h1 className={title}>
-      {PROFESSION}
-    </h1>
-    <h3 className={subtitle}>
-      I design websites that help businesses
-      {' '}
-      <Flip top>
-        <span className={green}>
-          grow
-        </span>
-      </Flip>
-      .
-    </h3>
+    <Flip top>
+      <h1 className={title}>
+        {PROFESSION}
+      </h1>
+    </Flip>
+    <Flip top>
+      <h3 className={subtitle}>
+        I design websites that help businesses grow.
+      </h3>
+    </Flip>
+
     <img
       src={avatarImg}
+      alt={WEBSITE_TITLE}
+      className={avatar}
+    />
+    <img
+      src={headerImg}
       alt={WEBSITE_TITLE}
       className={image}
     />
