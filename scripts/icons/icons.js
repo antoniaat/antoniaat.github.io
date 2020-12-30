0bd5f3ba3d937ed05644dca9c5364c38e399e5d3
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-30 15:06:22 +0200

    Added icons.js

---
+++ scripts/icons/icons.js
@@ -0,0 +1,9 @@
+import freelancerIcon from '../../assets/icons/freelancer.svg';
+import frontEndDeveloperIcon from '../../assets/icons/frontend.svg';
+import trainerIcon from '../../assets/icons/trainer.svg';
+
+export {
+  freelancerIcon,
+  frontEndDeveloperIcon,
+  trainerIcon,
+};
