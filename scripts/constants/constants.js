dcb563ed0d2951c20415a8be206d60993d6467fc
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-30 11:18:07 +0200

    Added common constants

--- scripts/constants/constants.js
+++ scripts/constants/constants.js
@@ -1,6 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 const CONTACT_EMAIL = 'antoniyaata@gmail.com';
 
+const PROFESSION = '[\'Front-end Developer\', \'Technical Trainer\', \'Freelancer\']';
+
+const WEBSITE_TITLE = 'Antonia Atanasova - Portfolio';
+
 export {
   CONTACT_EMAIL,
+  PROFESSION,
+  WEBSITE_TITLE,
 };
