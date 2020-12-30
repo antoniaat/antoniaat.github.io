c7ca3bc605313a9f5ef7681122321c99a044e4d1
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-30 10:52:39 +0200

    Fixed layout styles to use CSS modules

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,11 +1,12 @@
 import { node } from 'prop-types';
+import { wrapper } from '../styles/components/layout.module.scss';
 
 const Layout = ({ children }) => (
-  <>
+  <div className={wrapper}>
     {/* <Header /> */}
     {children}
     {/* <Footer /> */}
-  </>
+  </div>
 );
 
 Layout.propTypes = {
