a16bc2a20fda1a945d162b732e71986a296207bf
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-30 11:18:16 +0200

    Added header component

---
+++ components/header/header.jsx
@@ -0,0 +1,15 @@
+import { wrapper, title, subtitle } from '../../styles/components/header/header.module.scss';
+import { PROFESSION } from '../../scripts/constants/constants';
+
+const Header = () => {
+  <header className={wrapper}>
+    <h1 className={title}>
+      {PROFESSION}
+    </h1>
+    <h3 className={subtitle}>
+      I design websites that help businesses grow.
+    </h3>
+  </header>;
+};
+
+export default Header;
