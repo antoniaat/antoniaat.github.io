b33323ac893b35e59427fe245a229a87fad37825
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-31 15:52:49 +0200

    Removed the github workflows

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 const withImages = require('next-images');
 
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = withImages({
-  //  Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/antoniaat/antoniaat.github.io/gh-pages' : '',
-});
+module.exports = withImages({});
