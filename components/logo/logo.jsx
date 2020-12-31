e7526ae7567fdb0f74d4ff2c80de6a9f83840ad7
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-31 11:23:38 +0200

    Extended logo component to have light and dark type

--- components/logo/logo.jsx
+++ components/logo/logo.jsx
@@ -1,10 +1,30 @@
-import { logo, initials } from '../../styles/components/logo/logo.module.scss';
+import { oneOfType } from 'prop-types';
+import { LOGO_TYPES } from '../../scripts/constants/constants';
+import { getClassName } from '../../scripts/utils/common';
+import styles from '../../styles/components/logo/logo.module.scss';
 
-const Logo = () => (
-  <article className={logo}>
-    antonia
-    <span className={initials}>AT</span>
-  </article>
-);
+const Logo = ({ type }) => {
+  const combinedClassName = getClassName({
+    [styles.logo]: true,
+    [styles[type]]: true,
+  });
+
+  const { initials } = styles;
+
+  return (
+    <article className={combinedClassName}>
+      antonia
+      <span className={initials}>AT</span>
+    </article>
+  );
+};
+
+Logo.propTypes = {
+  type: oneOfType(LOGO_TYPES),
+};
+
+Logo.defaultProps = {
+  type: 'dark',
+};
 
 export default Logo;
