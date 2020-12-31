5fb3aa56381383b813bf3d5705f26e1427d8db35
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-31 14:32:06 +0200

    Added CDN to serve the static files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 const withImages = require('next-images');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withImages({});
+
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/antoniaat/antoniaat.github.io/gh-pages' : '',
+};
