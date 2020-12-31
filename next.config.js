6db8f6291cd3691328b498c770259d21b41fdff4
Antonia Atanasova <antoniyaata@gmail.com>
2020-12-31 15:16:20 +0200

    Fixed the module exports in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const withImages = require('next-images');
 
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = withImages({});
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
+module.exports = withImages({
+  //  Use the CDN in production and localhost for development.
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/antoniaat/antoniaat.github.io/gh-pages' : '',
-};
+});
