292198d0e28b48293e4d7b081557b59f1f8faa85
Antonia Atanasova <antoniyaata@gmail.com>
2022-09-19 11:41:36 +0300

    Removed unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import { Layout } from '../components/layout';
 import { Header } from '../components/header/header';
 import { About } from '../components/about/about';
-import { Skills } from '../components/skills/skills';
-import { Collaboration } from '../components/collaboration/collaboration';
 
 const Home = () => (
 	<Layout>
