80882e16bcfbb84c7b60dfabec5f7e875f85a836
Antonia Atanasova <antoniyaata@gmail.com>
2022-09-19 10:25:51 +0300

    Saved the chosen theme in LS

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { WEBSITE_TITLE } from '../scripts/constants/global';
 import { Navigation } from './navigation/navigation';
@@ -9,7 +9,23 @@ import { ThemeProvider } from 'styled-components';
 export const Layout = ({ children }) => {
 	const [theme, setTheme] = useState('light');
 	const isDarkTheme = theme === 'dark';
-	const toggleTheme = () => setTheme(isDarkTheme ? 'light' : 'dark');
+	
+	const toggleTheme = () => {
+		const updatedTheme = isDarkTheme ? 'light' : 'dark';
+		setTheme(updatedTheme);
+		localStorage.setItem('theme', updatedTheme);
+	};
+
+	useEffect(() => {
+		const savedTheme = localStorage.getItem('theme');
+		const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+		if (savedTheme && ['dark', 'light'].includes(savedTheme)) {
+			setTheme(savedTheme);
+		} else if (prefersDark) {
+			setTheme('dark');
+		}
+	}, []);
 
 	return (
 		<>
